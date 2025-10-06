932d3439a2afebc84389f909ef5b5e36bda842e2
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-10-06 09:12:22 -0300

    feat: added models

---
+++ models/movement.js
@@ -0,0 +1,27 @@
+const mongoose = require("../modules/mongoose");
+
+const movementSchema = new mongoose.Schema({
+    productId: {
+        type: Number,
+        required: true,
+    },
+
+    type: {
+        type: Boolean,
+        required: true,
+    },
+
+    quantity: {
+        type: Number,
+        required: true,
+    },
+
+    date: {
+        type: Date,
+        required: true,
+    },
+});
+
+const Movement = mongoose.model("movement", movementSchema);
+
+module.exports = { Movement };
